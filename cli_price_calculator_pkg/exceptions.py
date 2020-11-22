9231d71c806da1d460ff027281996fdd728ca0bc
Redbubble Applicant <me@example.com>
2020-11-22 19:28:27 +1100

    Add custom exceptions - Add a parent PriceCalcException with basic exception functionality to inherit from - Add CLIArgumentException for problems during retrieving arguments from CLI

---
+++ cli_price_calculator_pkg/exceptions.py
@@ -0,0 +1,22 @@
+class PriceCalcException(Exception):
+    
+    def __init__(self, message = None):
+        if message:
+            self.__message = f"{self.__class__.__name__}: {message}"
+        else:
+            self.__message = f"{self.__class__.__name__} has been raised"
+
+    def __str__(self):
+        return self.__message
+
+    @property
+    def message(self):
+        return self.__message
+
+    @message.setter
+    def message(self, value):
+        self.__message = value
+
+class CLIArgumentException(PriceCalcException):
+    def __init__(self, message = None):
+        super().__init__(message = message)
