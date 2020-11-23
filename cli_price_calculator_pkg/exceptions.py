a5bc17a264eced8878f3b2107e4aea6134cf0dc7
Redbubble Applicant <me@example.com>
2020-11-24 00:25:05 +1100

    Add doctrings for exceptions.py

--- cli_price_calculator_pkg/exceptions.py
+++ cli_price_calculator_pkg/exceptions.py
@@ -6,20 +6,53 @@ class PriceCalcException(Exception):
     Generic Exception class for PriceCalculator.
     '''
     def __init__(self, message = None):
+        '''
+        Constructor for Cart - intialise message string.
+
+        Args:
+            (str): Return message for the exception when raised.
+        Returns:
+            None.
+        '''
         if message:
             self.__message = f"{self.__class__.__name__}: {message}"
         else:
+            # Default message
             self.__message = f"{self.__class__.__name__} has been raised"
 
     def __str__(self):
+        '''
+        String representation of Exception -> the message value.
+
+        Args:
+            (self)
+        Returns
+            (str): Exception message
+        '''
         return self.__message
 
     @property
     def message(self):
+        '''
+        Property-based Getter for message.
+
+        Args:
+            (self)
+        Returns
+            (str): Exception message.
+        '''
         return self.__message
 
     @message.setter
     def message(self, value):
+        '''
+        Property-based Setter for message.
+
+        Args:
+            value (str): New message
+        Returns
+            None.
+        '''
         self.__message = value
 
 class CLIArgumentException(PriceCalcException):
@@ -31,7 +64,7 @@ class CLIArgumentException(PriceCalcException):
 class SchemaException(PriceCalcException):
     '''
     Used to represent exceptions related to incompatible schemas for the input
-    JSON files. 
+    JSON files. Like, duplicate products in base-prices with different prices.
     '''
     def __init__(self, message = None):
         super().__init__(message = message)
